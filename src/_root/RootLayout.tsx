41bf0f8ba7357ef16fdaf3fdde3f970a18ded87a
Raymond Liang <raymondliang2015@gmail.com>
2024-07-08 00:26:15 -0700

    add topbar, sidebar, bottombar

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,7 +1,18 @@
+import BottomBar from "@/components/shared/BottomBar"
+import LeftSidebar from "@/components/shared/LeftSidebar"
+import Topbar from "@/components/shared/Topbar"
+import { Outlet } from "react-router-dom"
 
 const RootLayout = () => {
     return (
-        <div>RootLayout</div>
+        <div className="w-full md:flex">
+            <Topbar />
+            <LeftSidebar />
+            <section className="flex flex-1 h-full">
+                <Outlet></Outlet>
+            </section>
+            <BottomBar />
+        </div>
     )
 }
 
